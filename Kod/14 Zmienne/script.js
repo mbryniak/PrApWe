71c25f46b333f1df1e121a4a238ec8ccd3519a9d
mmbryniak <mbryniak@student.wszib.edu.pl>
2024-01-28 10:17:28 +0100

    28012024

---
+++ Kod/14 Zmienne/script.js
@@ -0,0 +1,15 @@
+let numericVal = -5;
+let stringVal = 'Wartość';
+let booleanVal = false;
+
+console.log(numericVal);
+console.log(stringVal);
+console.log(booleanVal);
+
+console.log('-----------------------------------');
+
+console.log(typeof numericVal);
+console.log(typeof stringVal);
+console.log(typeof booleanVal);
+
+console.log('------------------------------------');
