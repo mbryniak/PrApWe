3569e1a1628b89120dbe59e30373a0a3967be112
mmbryniak <mbryniak@student.wszib.edu.pl>
2024-02-22 23:16:41 +0100

    28012024

--- Kod/14 Zmienne/script.js
+++ Kod/14 Zmienne/script.js
@@ -13,3 +13,9 @@ console.log(typeof stringVal);
 console.log(typeof booleanVal);
 
 console.log('------------------------------------');
+
+let sum = '1' + 1;
+console.log(sum);
+
+let diff = '1' - 1;
+console.log(diff);
